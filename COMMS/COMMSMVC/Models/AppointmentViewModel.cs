bab06ff1194304a28fdb8ce732f30da7614db598
杨思琪 <754868159@qq.com>
2026-03-09 20:15:31 +0800

    就诊管理页面

---
+++ COMMS/COMMSMVC/Models/AppointmentViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace COMMSMVC.Models
+{
+    public class AppointmentViewModel
+    {
+
+            public int AppointmentID { get; set; }
+            public int PatientID { get; set; }
+            public string PatientName { get; set; }  // 对应数据中的 Name 字段
+            public int ScheduleID { get; set; }
+            public string Status { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public string Remark { get; set; }
+        
+
+    }
+}
---
+++ COMMS/COMMSMVC/Models/CreateMedicationRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMMSMVC.Models
+{
+    public class CreateMedicationRequest
+    {
+        [Required(ErrorMessage = "药品名称不能为空")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "规格不能为空")]
+        [StringLength(50)]
+        public string Specification { get; set; }
+
+        [Required(ErrorMessage = "价格不能为空")]
+        [Range(0, double.MaxValue, ErrorMessage = "价格必须大于等于0")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "库存不能为空")]
+        [Range(0, int.MaxValue, ErrorMessage = "库存必须大于等于0")]
+        public int Stock { get; set; }
+        public bool IsActive { get; set; } 
+    }
+}
