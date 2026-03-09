919189d920609078af5525988ff00acacae908bf
杨思琪 <754868159@qq.com>
2026-03-10 00:24:17 +0800

    hardcode 前端界面，查看患者信息等

--- COMMS/COMMSMVC/Models/AppointmentViewModel.cs
+++ COMMS/COMMSMVC/Models/AppointmentViewModel.cs
@@ -5,12 +5,16 @@
 
             public int AppointmentID { get; set; }
             public int PatientID { get; set; }
-            public string PatientName { get; set; }  // 对应数据中的 Name 字段
+            public string PatientName { get; set; }//患者姓名
             public int ScheduleID { get; set; }
             public string Status { get; set; }
             public DateTime CreatedAt { get; set; }
             public string Remark { get; set; }
+            public string TimeSlot { get; set; }
+            public int DoctorID { get; set; }
+            public string DoctorName { get; set; }
         
+
 
     }
 }
