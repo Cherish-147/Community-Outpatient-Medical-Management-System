867db5a04017389dbb664d8b4f650409e39dad1f
杨思琪 <754868159@qq.com>
2026-03-29 19:07:20 +0800

    药品统计图

---
+++ COMMS/COMMSMVC/Models/Statistics/MedicationChartViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace COMMSMVC.Models.Statistics
+{
+    public class MedicationChartViewModel
+    {
+        public List<string> Names { get; set; }
+        public List<int> Stocks { get; set; }
+        public List<decimal> Prices { get; set; }
+    }
+}
